4ce0de468cab5f65afa66eff82f23133b849ae1f
Sai Krishna Koduri <s561232@nwmissouri.edu>
2023-11-08 22:19:03 -0600

    Implement create

--- controllers/bike.js
+++ controllers/bike.js
@@ -7,10 +7,26 @@ res.send('NOT IMPLEMENTED: Bike list');
 exports.bike_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Bike detail: ' + req.params.id);
 };
-// Handle Bike create on POST.
-exports.bike_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Bike create POST');
-};
+// Handle Costume create on POST.
+exports.bike_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Bike();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.bike_name = req.body.bike_name;
+    document.bike_mielage = req.body.bike_mielage;
+    document.bike_maker = req.body.bike_maker;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
 // Handle Bike delete form on DELETE.
 exports.bike_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Bike delete DELETE ' + req.params.id);
