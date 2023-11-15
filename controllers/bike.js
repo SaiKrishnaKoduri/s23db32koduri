4bdfd597c3db494a468ed350b4e1279ef39d3645
Sai Krishna Koduri <s561232@nwmissouri.edu>
2023-11-14 19:26:55 -0600

    deleted instance

--- controllers/bike.js
+++ controllers/bike.js
@@ -1,12 +1,21 @@
+const bike = require('../models/bike');
 var Bike = require('../models/bike');
 // List of all Bikes
 exports.bike_list = function(req, res) {
 res.send('NOT IMPLEMENTED: Bike list');
 };
 // for a specific Bike.
-exports.bike_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Bike detail: ' + req.params.id);
-};
+exports.bike_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Bike.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
 // Handle Costume create on POST.
 exports.bike_create_post = async function(req, res) {
     console.log(req.body)
@@ -27,13 +36,39 @@ exports.bike_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
-// Handle Bike delete form on DELETE.
-exports.bike_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Bike delete DELETE ' + req.params.id);
-};
-// Handle Bike update form on PUT.
-exports.bike_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Bike update PUT' + req.params.id);
+    
+// Handle Bike delete on DELETE.
+exports.bike_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Bike.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+
+// Handle Costume update form on PUT.
+exports.bike_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Bike.findById( req.params.id)
+// Do updates of properties
+if(req.body.bike_name) toUpdate.bike_name = req.body.bike_name;
+if(req.body.bike_mielage) toUpdate.bike_mielage = req.body.bike_mielage;
+if(req.body.bike_maker) toUpdate.bike_maker = req.body.bike_maker;
+if(req.body.bike_year) toUpdate.bike_year = req.body.bike_year;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
 
 // List of all Bike
--- routes/bike.js
+++ routes/bike.js
@@ -4,3 +4,5 @@ var router = express.Router();
 /* GET bike */
 router.get('/', bike_controlers.bike_view_all_Page );
 module.exports = router;
+// GET request for one costume.
+router.get('/bike/:id', bike_controlers.bike_detail);
