58c02ffa4b79d2857faa1a149b2b3255bc1283d1
Sai Krishna Koduri <s561232@nwmissouri.edu>
2023-11-17 11:54:45 -0600

    modified app.js

--- app.js
+++ app.js
@@ -4,6 +4,30 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var Bike = require("./models/bike");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
+
+
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
@@ -14,8 +38,9 @@ mongoose.connect('mongodb+srv://SaiKrishnaKoduri:Irudok98@cluster0.r36oy6p.mongo
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -36,6 +61,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport config
+// Use the existing connection
+// The Account model
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bike', bikeRouter);
@@ -44,12 +85,12 @@ app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -60,34 +101,37 @@ app.use(function(err, req, res, next) {
 });
 
 // We can seed the collection if needed on server start
-async function recreateDB(){
-// Delete everything
-await Bike.deleteMany();
-let instance1 = new Bike({ bike_name: 'R15', bike_mielage: 25, bike_maker: 'Yamaha', bike_year: 2012 });
-instance1.save().then(doc=>{
-console.log("First object saved")}
-).catch(err=>{
-console.error(err)
-});
+async function recreateDB() {
+  // Delete everything
+  await Bike.deleteMany();
+  let instance1 = new Bike({ bike_name: 'R15', bike_mielage: 25, bike_maker: 'Yamaha', bike_year: 2012 });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
-let instance2 = new
-Bike({ bike_name: 'Splender', bike_mielage: 40, bike_maker: 'Hero', bike_year: 2014 });
-instance2.save().then(doc=>{
-console.log("Second object saved")}
-).catch(err=>{
-console.error(err)
-});
+  let instance2 = new
+    Bike({ bike_name: 'Splender', bike_mielage: 40, bike_maker: 'Hero', bike_year: 2014 });
+  instance2.save().then(doc => {
+    console.log("Second object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
-let instance3 = new
-Bike({ bike_name: 'Heavy Duty', bike_mielage: 60, bike_maker: 'TVS', bike_year: 2016 });
-instance3.save().then(doc=>{
-console.log("Third object saved")}
-).catch(err=>{
-console.error(err)
-});
+  let instance3 = new
+    Bike({ bike_name: 'Heavy Duty', bike_mielage: 60, bike_maker: 'TVS', bike_year: 2016 });
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
 }
 let reseed = true;
-if (reseed) {recreateDB();}
+if (reseed) { recreateDB(); }
 
 module.exports = app;
