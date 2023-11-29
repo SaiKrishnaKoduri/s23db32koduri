5679db62d8c385bcbff965572878665825cb8167
Sai Krishna Koduri <s561232@nwmissouri.edu>
2023-11-29 15:57:36 -0600

    added login and register pages

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ app.use(passport.session());
 // passport config
 // Use the existing connection
 // The Account model
+var Account =require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
