688ba56197d7fff0146d97e01a32bf2f7b5faea1
Sai Krishna Koduri <s561232@nwmissouri.edu>
2023-11-29 21:06:18 -0600

    added buttons for create detail and delete

--- models/bike.js
+++ models/bike.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
 const bikeSchema = mongoose.Schema({
-    bike_name: String,
-    bike_mielage: Number,
+bike_name: String,
+bike_mielage: { type: Number,
+    min: 10,
+    max: 100,
+},
     bike_maker: String,
     bike_year: Number
 })
--- routes/bike.js
+++ routes/bike.js
@@ -1,6 +1,12 @@
 var express = require('express');
 const bike_controlers= require('../controllers/bike');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 /* GET bike */
 router.get('/', bike_controlers.bike_view_all_Page );
 module.exports = router;
@@ -11,6 +17,6 @@ router.get('/detail', bike_controlers.bike_view_one_Page);
 /* GET create costume page */
 router.get('/create', bike_controlers.bike_create_Page);
 /* GET create update page */
-router.get('/update', bike_controlers.bike_update_Page);
+router.get('/update',secured, bike_controlers.bike_update_Page);
 /* GET delete costume page */
 router.get('/delete', bike_controlers.bike_delete_Page);
